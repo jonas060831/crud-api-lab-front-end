7d9e73ff7f2e61b360cbd7378b56c9122882a2fb
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2024-12-09 23:04:50 -0800

    plan approach

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 
 import './App.css'
 
+
+{/*
+
+  PLAN
+
+C TAB NAME = +  
+R TAB NAME = Home
+U CLICKING THE LI ON THE LIST OF MOVIES WILL SHOW A FORM POPULATED WITH VALUES BASE ON THE ID
+D IN THE FORM UPDATE THERE IS A BUTTON TO SEND A DELETE REQUEST
+
+*/}
+
+
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+
+
+        
+      
+    </div>
   )
 }
 
